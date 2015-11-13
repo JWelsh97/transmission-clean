90d9f9a7420c03cd8a92abe3142539b72e5ebf8b
JWelsh97 <jackwelsh@protonmail.com>
2015-11-13 22:33:05 +0000

    Fixed PEP8 issues

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import sys
 import transmission
 
 if '-print' in sys.argv:
-     map(transmission.print_torrent, transmission.get_torrentlist())
+    map(transmission.print_torrent, transmission.get_torrentlist())
 elif '-clean' in sys.argv:
-    map(lambda t: transmission.remove_torrent(transmission.print_torrent(t)), \
-    filter(lambda t: transmission.old_torrent(t) or transmission.seeded_torrent(t), transmission.get_torrentlist()))
+    map(lambda t: transmission.remove_torrent(transmission.print_torrent(t)),
+        filter(lambda t: transmission.old_torrent(t) or
+        transmission.seeded_torrent(t), transmission.get_torrentlist()))
 
