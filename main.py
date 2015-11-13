2266f5abfd9bc454ded79b938ca4b59de48fc713
JWelsh97 <jackwelsh@protonmail.com>
2015-11-13 22:37:40 +0000

    Added function to read a yaml config

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 import sys
+import os
+import yaml
 import transmission
+
+
+def read_config():
+    """
+    Reads the YAML config file
+    """
+    path = os.path.dirname(os.path.realpath(sys.argv[0]))
+    with open('%s/config.yaml' % path, 'r') as f:
+        conf = yaml.load(f)
+    return conf
+
 
 if '-print' in sys.argv:
     map(transmission.print_torrent, transmission.get_torrentlist())
