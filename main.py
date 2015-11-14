05522558e8ac202d9fc353961182412de1565365
JWelsh97 <jackwelsh@protonmail.com>
2015-11-14 03:18:15 +0000

    Added --list-devices command + short commands, --clean will now push a notification

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import sys
 import os
 import yaml
-import transmission
+import datetime
+from pushbullet import PushBullet
+import transmission as tr
 
 
 def read_config():
@@ -14,11 +16,24 @@ def read_config():
     return conf
 
 conf = read_config()
+pb = PushBullet(conf['pushbullet']['access_token'])
+devices = conf['pushbullet']['devices']
+dt_time = datetime.datetime.now().strftime('%I:%M%p %d/%m/%y')
 
-if '-print' in sys.argv:
-    map(transmission.print_torrent, transmission.get_torrentlist(conf))
-elif '-clean' in sys.argv:
-    map(lambda t: transmission.remove_torrent(transmission.print_torrent(t)),
-        filter(lambda t: transmission.old_torrent(conf, t) or
-        transmission.seeded_torrent(conf, t), transmission.get_torrentlist(conf)))
+if '-p' in sys.argv or '--print' in sys.argv:
+    map(tr.print_torrent, tr.get_torrentlist(conf))
+elif '-l' in sys.argv or '--list-devices' in sys.argv:
+    for idx, device in enumerate(pb.get_devices()):
+        print('[{0}] {1}: {2}'.format(idx, *device))
+elif '-c' in sys.argv or '--clean' in sys.argv:
+    complete_torrents = filter(lambda t: tr.old_torrent(conf, t) or tr.seeded_torrent(conf, t),
+                               tr.get_torrentlist(conf))
+
+    # Send a push for each torrent to be remove
+    map(lambda t: pb.push_note("Torrent Removed",
+                               "Torrent {0} has been removed at {1}".format(t['name'], dt_time),
+                               devices), complete_torrents)
+
+    # Remove and print torrents
+    map(lambda t: tr.remove_torrent(conf, tr.print_torrent(t)), complete_torrents)
 
