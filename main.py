31c2a991d10b50e7fe04c7e53753287bdad92400
JWelsh97 <jackwelsh@protonmail.com>
2015-11-14 19:42:58 +0000

    Added --add-device and accompanied for config functions in config module

--- main.py
+++ main.py
@@ -1,39 +1,66 @@
 import sys
-import os
-import yaml
 import datetime
+import config
 from pushbullet import PushBullet
-import transmission as tr
+from transmission import Transmission
 
 
-def read_config():
+def add_device(pb, conf, dev_num):
     """
-    Reads the YAML config file
+    Adds a device identity to config
+    using --add-device or -a command
     """
-    path = os.path.dirname(os.path.realpath(sys.argv[0]))
-    with open('%s/config.yaml' % path, 'r') as f:
-        conf = yaml.load(f)
-    return conf
+    dev_lst = pb.get_devices()
+    try:
+        dev_name, dev_id = dev_lst[dev_num]
+    except:
+        print('Invalid device')
+        return
 
-conf = read_config()
+    if 'devices' not in conf['pushbullet']:
+        conf['pushbullet']['devices'] = []
+
+    if not isinstance(conf['pushbullet']['devices'], list):
+        conf['pushbullet']['devices'] = []
+
+    if dev_id in conf['pushbullet']['devices']:
+        print('%s is already listed' % dev_name)
+    else:
+        conf['pushbullet']['devices'].append(dev_id)
+        config.write_config(conf)
+        print('Added %s' % dev_name)
+
+conf = config.read_config()
+tr = Transmission(**conf['transmission'])
 pb = PushBullet(conf['pushbullet']['access_token'])
 devices = conf['pushbullet']['devices']
 dt_time = datetime.datetime.now().strftime('%I:%M%p %d/%m/%y')
 
 if '-p' in sys.argv or '--print' in sys.argv:
-    map(tr.print_torrent, tr.get_torrentlist(conf))
+    map(tr.print_torrent, tr.get_torrentlist())
+
 elif '-l' in sys.argv or '--list-devices' in sys.argv:
     for idx, device in enumerate(pb.get_devices()):
         print('[{0}] {1}: {2}'.format(idx, *device))
+
+elif '-a' in sys.argv or '--add-device' in sys.argv:
+    try:
+        dev_num = int(sys.argv[2])
+    except:
+        print('Incorrect usage')
+        print('Example: -a/--add-device NUMBER')
+        sys.exit()
+    add_device(pb, conf, dev_num)
+
 elif '-c' in sys.argv or '--clean' in sys.argv:
-    complete_torrents = filter(lambda t: tr.old_torrent(conf, t) or tr.seeded_torrent(conf, t),
-                               tr.get_torrentlist(conf))
+    complete_torrents = filter(lambda t: tr.old_torrent(t) or tr.seeded_torrent(t),
+                               tr.get_torrentlist())
 
     # Send a push for each torrent to be remove
-    map(lambda t: pb.push_note("Torrent Removed",
-                               "Torrent {0} has been removed at {1}".format(t['name'], dt_time),
+    map(lambda t: pb.push_note('Torrent Removed',
+                               "Torrent '{0}' was removed at {1}".format(t['name'], dt_time),
                                devices), complete_torrents)
 
     # Remove and print torrents
-    map(lambda t: tr.remove_torrent(conf, tr.print_torrent(t)), complete_torrents)
+    map(lambda t: tr.remove_torrent(tr.print_torrent(t)), complete_torrents)
 
